7723eaa6dcc306d29f17bb177c87634e99fd661c
Mariahsal <89291278+Mariahsal@users.noreply.github.com>
2022-12-15 14:35:24 -0800

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -5,21 +5,21 @@
  **/
 class Solution {
 public:
-    Node* cloneGraph(Node* node) { // Stores the address of any variable of type 'node' 
+    Node* cloneGraph(Node* node) { //Stores the address of any variable of type 'node' 
         if (!node) { //Check if root is Null
             return NULL;
         }
-        Node* copy = new Node(node -> val, {});
-        copies[node] = copy;
+        Node* copy = new Node(node -> val, {}); //Allocate a new node that has the same value as our input
+        copies[node] = copy; //Assigning the value in the hash table at key 'node'
         queue<Node*> todo;
-        todo.push(node);
+        todo.push(node); //Push the node onto the queue
         while (!todo.empty()) {
-            Node* cur = todo.front();
-            todo.pop();
-            for (Node* neighbor : cur -> neighbors) {
-                if (copies.find(neighbor) == copies.end()) {
+            Node* cur = todo.front(); //Get the current node (Front of the queue)
+            todo.pop(); //Delete the current node off of our queue
+            for (Node* neighbor : cur -> neighbors) { //Looping thrugh all of the current node's neighbors
+                if (copies.find(neighbor) == copies.end()) { //Visit current node
                     copies[neighbor] = new Node(neighbor -> val, {});
-                    todo.push(neighbor);
+                    todo.push(neighbor); //Append n of current node onto neighbors
                 }
                 copies[cur] -> neighbors.push_back(copies[neighbor]);
             }
@@ -27,5 +27,5 @@ public:
         return copy;
     }
 private:
-    unordered_map<Node*, Node*> copies;
+    unordered_map<Node*, Node*> copies; //A hash table with Key as a Node and value also as a Node
 };
